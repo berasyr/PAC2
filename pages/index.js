7fc71a33423f564a6e555cffd168901b8f75eae2
berasyr <bfelouz@syr.edu>
2025-05-03 01:23:44 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pace - Your Rewards App</title>
+        <meta name="description" content="Pace - Earn rewards for your daily activities" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+    </>
+  );
+} 
